e51d65bd5c305f1d95671fd95fa8af6516c1991d
Leo <leo80103@gmail.com>
2024-08-14 21:39:11 +0800

    完成第一個練習

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,18 +1,18 @@
 import componentsImg from "./assets/components.png";
 import { useState } from "react";
-import { CORE_CONCEPTS } from "./data.js";
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handSelect(selectedButton) {
     // selectedButton => 'componts', 'jsx', 'props', 'state '
     console.log(selectedButton);
-    setSelectedTopic(`you are clicked ${selectedButton}`);
+    setSelectedTopic(selectedButton);
   }
 
   return (
@@ -22,22 +22,37 @@ function App() {
         <section id="core-concepts">
           <h2>Cord Concepts</h2>
           <ul>
-            {CORE_CONCEPTS.map((concept, index) => (
-              <CoreConcept key={index} {...concept} />
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
             ))}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handSelect("componts")}>
+            <TabButton isSelected={selectedTopic === "components"} onSelect={() => handSelect("components")}>
               Components
             </TabButton>
-            <TabButton onSelect={() => handSelect("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handSelect("props")}>Props</TabButton>
-            <TabButton onSelect={() => handSelect("state")}>State</TabButton>
+            <TabButton isSelected={selectedTopic === "jsx"} onSelect={() => handSelect("jsx")}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === "props"} onSelect={() => handSelect("props")}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === "state"} onSelect={() => handSelect("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          {/* && 如果前面是 true 就會輸出後面的 */}
+          {!selectedTopic && <p>Please select a topic</p> }
+          {selectedTopic && (
+                <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>
+                  {EXAMPLES[selectedTopic].description}
+                </p>
+                <pre>
+                  <code>
+                  {EXAMPLES[selectedTopic].code}
+                  </code>
+                </pre>
+              </div>
+            ) 
+          }
         </section>
       </main>
     </div>
