cf198e7efa1fb006dad079ec5984216d81012ff7
Leo <leo80103@example.com>
2024-08-15 17:01:30 +0800

    持續課程

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,59 +1,17 @@
-import componentsImg from "./assets/components.png";
-import { useState } from "react";
-import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
-
 import Header from "./components/Header/Header.jsx";
-import CoreConcept from "./components/CoreConcept.jsx";
-import TabButton from "./components/TabButton.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
+
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
 
-  function handSelect(selectedButton) {
-    // selectedButton => 'componts', 'jsx', 'props', 'state '
-    console.log(selectedButton);
-    setSelectedTopic(selectedButton);
-  }
 
   return (
     <div>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Cord Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} {...conceptItem} />
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected={selectedTopic === "components"} onSelect={() => handSelect("components")}>
-              Components
-            </TabButton>
-            <TabButton isSelected={selectedTopic === "jsx"} onSelect={() => handSelect("jsx")}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic === "props"} onSelect={() => handSelect("props")}>Props</TabButton>
-            <TabButton isSelected={selectedTopic === "state"} onSelect={() => handSelect("state")}>State</TabButton>
-          </menu>
-          {/* && 如果前面是 true 就會輸出後面的 */}
-          {!selectedTopic && <p>Please select a topic</p> }
-          {selectedTopic && (
-                <div id="tab-content">
-                <h3>{EXAMPLES[selectedTopic].title}</h3>
-                <p>
-                  {EXAMPLES[selectedTopic].description}
-                </p>
-                <pre>
-                  <code>
-                  {EXAMPLES[selectedTopic].code}
-                  </code>
-                </pre>
-              </div>
-            ) 
-          }
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
     </div>
   );
---
+++ 01-starting-project/src/components/Examples.jsx
@@ -0,0 +1,65 @@
+import {useState} from "react"
+import { EXAMPLES } from "../data.js";
+
+import Section from "./Section.jsx";
+
+import Tabs from "./TabButton.jsx";
+import TabButton from "./TabButton";
+
+export default function Examples() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handSelect(selectedButton) {
+    // selectedButton => 'componts', 'jsx', 'props', 'state '
+    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+  }
+
+  return (
+    <Section title="Examples" id="examples">
+      <Tabs buttons={
+        <>
+        <TabButton
+          isSelected={selectedTopic === "components"}
+          onClick={() => handSelect("components")}
+        >
+          Components
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "jsx"}
+          onClick={() => handSelect("jsx")}
+        >
+          JSX
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "props"}
+          onClick={() => handSelect("props")}
+        >
+          Props
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "state"}
+          onClick={() => handSelect("state")}
+        >
+          State
+        </TabButton>
+        </>
+      }>
+
+        {/* && 如果前面是 true 就會輸出後面的 */}
+        {!selectedTopic && <p>Please select a topic</p>}
+        {selectedTopic && (
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
+        )}
+      </Tabs>
+
+
+    </Section>
+  )
+}
