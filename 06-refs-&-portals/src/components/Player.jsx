7505d8b7606442257dc32c3d2bbc5d1e3532a31f
Aruminiya <leo80103@gmail.com>
2024-08-21 17:45:08 +0800

    猜時間遊戲進度執行中

--- 06-refs-&-portals/src/components/Player.jsx
+++ 06-refs-&-portals/src/components/Player.jsx
@@ -1,10 +1,26 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+
+  const [enterPlayerName, setEnterPlayerName] = useState(null);
+
+  function handleClick() {
+    // setSubmitted(()=>true);
+    setEnterPlayerName(playerName.current.value);
+    playerName.current.value = ''
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enterPlayerName ?? 'unknown entity'}</h2>
+      {/* 如果 enterPlayerName 是 true 輸出 enterPlayerName ，反之 輸出 'unknown entity' */}
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input 
+          ref={playerName}
+          type="text"
+        />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
