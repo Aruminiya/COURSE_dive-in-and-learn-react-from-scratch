04954b74dc1a203d3c251a90e7c4d92a74e29d9f
Leo <leo80103@gmail.com>
2024-08-24 15:37:34 +0800

    切版和基本邏輯架設

--- 07-practice-project-project-management/src/App.jsx
+++ 07-practice-project-project-management/src/App.jsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
+
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoprojectSelected";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    currentAction: 'notion=selected',
+    selectedProjectId: undefined,
+    project: []
+  });
+
+  function handleStartsAddProject() {
+    setProjectState(prevState => ({
+      ...prevState,
+      selectedProjectId: null,
+
+    }));
+  };
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartsAddProject={handleStartsAddProject} />
+  }
+
   return (
-    <main className="h-screen my-8">
-      <ProjectsSidebar />
+    <main className="h-screen my-8 flex gap-2">
+      <ProjectsSidebar onStartsAddProject={handleStartsAddProject} />
+      {content}
     </main>
   );
 }
