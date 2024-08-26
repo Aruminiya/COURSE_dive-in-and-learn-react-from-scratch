59fd8ced29038b80efd8c8da95305aaef9e7e6c2
Aruminiya <leo80103@gmail.com>
2024-08-26 15:28:54 +0800

    任務欄位執行中

--- 07-practice-project-project-management/src/components/SelectedProject.jsx
+++ 07-practice-project-project-management/src/components/SelectedProject.jsx
@@ -1,3 +1,5 @@
+import Tasks from "./Tasks";
+
 export default function SelectedProject({project, onDelete}) {
   const formattedDate = new Date(project.dueDate).toLocaleDateString('zh-TW',{
     year: 'numeric',
@@ -7,7 +9,7 @@ export default function SelectedProject({project, onDelete}) {
 
   return (
     <div className="w-[35rem] mt-16">
-      <header className="pb-4 mb-4 border-stone-300">
+      <header className="pb-4 mb-4 border-b-2 border-stone-300">
         <div className="flex items-center justify-between">
           <h1 className="text-3xl font-bold text-stone-600 mb-2">
             {project.title}
@@ -17,9 +19,11 @@ export default function SelectedProject({project, onDelete}) {
           </button>
         </div>
         <p className="mb-4 text-stone-400">{formattedDate}</p>
-        <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
+        <p className="text-stone-600 whitespace-pre-wrap">
+          {project.description}
+        </p>
       </header>
-      TASKS
+      <Tasks />
     </div>
   );
 };
