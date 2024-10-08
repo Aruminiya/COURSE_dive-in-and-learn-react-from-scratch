1a48a7126a4e26a79d00db47b9b975ee8da8965e
Aruminiya <leo80103@gmail.com>
2024-10-08 12:08:56 +0800

    完成 13 課程

--- 13-http-requests/src/http.js
+++ 13-http-requests/src/http.js
@@ -9,8 +9,19 @@ export async function fetchAvailablePlaces() {
   return resData.places;
 };
 
+export async function fetchUserPlaces() {
+  const response = await fetch('http://localhost:3000/user-places');
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch user places');
+  }
+
+  return resData.places;
+};
+
 export async function updateUserPlaces(places) {
-  const response = await fetch('http://localhost:3000/places', {
+  const response = await fetch('http://localhost:3000/user-places', {
     method: 'PUT',
     body: JSON.stringify({places}),
     headers: {
@@ -24,4 +35,4 @@ export async function updateUserPlaces(places) {
   };
 
   return resData.message;
-}
+};
