f2e033f6fefb991f7a13e50c99fa5060857cb3b9
Aruminiya <leo80103@gmail.com>
2024-10-18 18:04:24 +0800

    結帳表單

--- 16-practice-project-food-order/src/components/Cart.jsx
+++ 16-practice-project-food-order/src/components/Cart.jsx
@@ -26,8 +26,8 @@ export default function Cart() {
         </ul>
         <p className="cart-total">Total: {currencyFormatter.format(cartTotal)}</p>
         <p className="modal-actions">
-              <Button textOnly onClick={hideCart}>Close</Button>
-              <Button onClick={showCheckout}>Go to Checkout</Button>
+          <Button textOnly onClick={hideCart}>Close</Button>
+          {items.length > 0 && <Button onClick={showCheckout}>Go to Checkout</Button>}
         </p>
       </>
     </Modal>
--- 16-practice-project-food-order/src/components/Checkout.jsx
+++ 16-practice-project-food-order/src/components/Checkout.jsx
@@ -4,19 +4,43 @@ import { CartContext } from "../store/CartContext";
 import { currencyFormatter } from "../util/formatting";
 import Button from "./UI/Button";
 import { UserProgressContext } from "../store/UserProgressContext";
+import Input from "./UI/Input";
+
 export default function Checkout() {
   const { items } = useContext(CartContext);
   const { userProgres, hideCheckout } = useContext(UserProgressContext);
 
+  const cartTotal = items.reduce((total, item) => {
+    return total + item.price * item.quantity;
+  }, 0);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+    hideCheckout();
+  };
+
   return (
     <Modal className="cart" open={userProgres === 'checkout'}>
-      <>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
-        <p>Your order is being processed.</p>
+        <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
+
+        <Input label="Full Name" id="full-name" type="text" />
+        <Input label="E-Mail Address" id="email" type="email" />
+        <Input label="Street" id="street" type="text" />
+        <div className="control-row">
+          <Input label="Postal Code" id="postal-code" type="text" />
+          <Input label="City" id="city" type="text" />
+        </div>
+        
         <p className="modal-actions">
-          <Button textOnly onClick={hideCheckout}>Close</Button>
+          <Button type="button" textOnly onClick={hideCheckout}>Close</Button>
+          <Button type="submit" >Submit Order</Button>
         </p>
-      </>
+      </form>
     </Modal>
   );
 };
