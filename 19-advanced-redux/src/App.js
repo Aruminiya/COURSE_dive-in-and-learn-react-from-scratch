00d332df37747d1a1a283f9a892e1d803b27e4a3
Leo <leo80103@gmail.com>
2024-12-01 12:46:20 +0800

    feat: 優化邏輯

--- 19-advanced-redux/src/App.js
+++ 19-advanced-redux/src/App.js
@@ -1,15 +1,66 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { uiActions } from './store/slices/uiSlices';
+import Notification from './components/UI/Notification';
+
+let isInitial = true;
 
 function App() {
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
+  const cartItems = useSelector((state) => state.cart.items);
+  const notification = useSelector((state) => state.ui.notification);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    const sendCartData = async () => {
+      try {
+        dispatch(uiActions.showNotification({
+          status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!',
+      }));
+
+      const response = await fetch('https://course-react-from-scratch-default-rtdb.asia-southeast1.firebasedatabase.app/cartItems.json', {
+        method: 'PUT',
+        body: JSON.stringify(cartItems),
+      });
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+          message: 'Sent cart data successfully!',
+        }));
+      } catch (error) {
+        dispatch(uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed.',
+        }));
+      }
+    };
+
+    sendCartData();
+  }, [cartItems, dispatch]);
+
   return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
+      <Layout>
+        {cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- 19-advanced-redux/src/components/Shop/Products.js
+++ 19-advanced-redux/src/components/Shop/Products.js
@@ -1,11 +1,6 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
-
-const productsData = [
-  { id: '1', title: 'Product 1', price: 100, description: 'This is a product 1' },
-  { id: '2', title: 'Product 2', price: 200, description: 'This is a product 2' },
-  { id: '3', title: 'Product 3', price: 300, description: 'This is a product 3' },
-];
+import productsData from './productsData.json';
 
 const Products = () => {
   return (
