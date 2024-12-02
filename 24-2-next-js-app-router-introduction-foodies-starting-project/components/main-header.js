134689af338df067ae1738f788854079a0e2bb05
Aruminiya <leo80103@gmail.com>
2024-12-02 15:01:17 +0800

    feat: 新增 MainHeader 組件

--- 24-2-next-js-app-router-introduction-foodies-starting-project/components/main-header.js
+++ 24-2-next-js-app-router-introduction-foodies-starting-project/components/main-header.js
@@ -1,7 +1,25 @@
+import Link from 'next/link';
+import LogoImg from '@/assets/logo.png';
+
 export default function MainHeader() {
   return (
     <header>
-      <h1>MainHeader</h1>
+      <Link href="/">
+        {/* next 的 圖像會儲存到 .src 的屬性下 */}
+        <img src={LogoImg.src} alt="A plate with food on it" /> 
+        NextLevel Food
+      </Link>
+
+      <nav>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+          <li>
+            <Link href="/community">Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
