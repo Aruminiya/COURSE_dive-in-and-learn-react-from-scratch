3683d8c3611f3212252ee7630514ac6bbf0b94e0
Aruminiya <leo80103@gmail.com>
2024-12-03 18:17:20 +0800

    feat: link 組件化

--- 24-2-next-js-app-router-introduction-foodies-starting-project/components/main-header/main-header.js
+++ 24-2-next-js-app-router-introduction-foodies-starting-project/components/main-header/main-header.js
@@ -1,10 +1,12 @@
 import Link from 'next/link';
+import NavLink from './nav-link';
 import Image from 'next/image';
 import LogoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
 import MainHeaderBackground from './main-header-background';
 
 export default function MainHeader() {
+
   return (
     <>
       <MainHeaderBackground />
@@ -17,10 +19,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals" >Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community" >Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
