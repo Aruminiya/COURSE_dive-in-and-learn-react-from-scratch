6d90e21b59bfec02303f1135b03e55772d4c5693
Aruminiya <leo80103@gmail.com>
2024-12-05 18:34:30 +0800

    feat: 添加錯誤頁面

--- 24-2-next-js-app-router-introduction-foodies-starting-project/lib/meals.js
+++ 24-2-next-js-app-router-introduction-foodies-starting-project/lib/meals.js
@@ -9,5 +9,6 @@ export async function getMeals() {
   */
   await new Promise((resolve) => setTimeout(resolve, 2000)); 
 
+  // throw new Error('Failed to fetch meals');
   return db.prepare('SELECT * FROM meals').all();
 }
