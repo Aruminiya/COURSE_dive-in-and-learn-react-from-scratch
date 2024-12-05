77a2b96785e6293b2fa7d966ca86f79ca2c68429
Aruminiya <leo80103@gmail.com>
2024-12-05 18:22:29 +0800

    feat: 使用 Suspense 來細分 Loading 狀態

--- 24-2-next-js-app-router-introduction-foodies-starting-project/app/meals/page.js
+++ 24-2-next-js-app-router-introduction-foodies-starting-project/app/meals/page.js
@@ -1,13 +1,20 @@
+import { Suspense } from 'react';
 import Link from 'next/link';
 import MealsGrid from '@/components/meals/meals-grid';
 import classes from './page.module.css';
 import { getMeals } from '@/lib/meals';
 
+async function Meals(){
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />
+}
+
 /*
 在 Next.js 中，頁面組件可以是異步的，因為 Next.js 在服務器端渲染時會等待這些異步操作完成，然後再將結果發送到客戶端。這樣的設計允許在渲染之前完成數據的獲取。
 */
-export default async function MealsPage() { 
-  const meals = await getMeals();
+export default function MealsPage() { 
+  
   return (
     <>
       <header className={classes.header}>
@@ -23,7 +30,9 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
